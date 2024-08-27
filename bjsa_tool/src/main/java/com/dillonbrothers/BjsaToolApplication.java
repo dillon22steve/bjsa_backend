50cbeee0b75483a26de82760be784d6aa187340e
dillon22steve <std42400@uga.edu>
2024-08-27 07:27:57 -0400

    Added a get request handler in the controller

--- bjsa_tool/src/main/java/com/dillonbrothers/BjsaToolApplication.java
+++ bjsa_tool/src/main/java/com/dillonbrothers/BjsaToolApplication.java
@@ -10,33 +10,6 @@ public class BjsaToolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BjsaToolApplication.class, args);
-
-		String startingHand = "6, 6";
-		// char dealerUpCard = '9';
-		// BigInteger numExecutions = new BigInteger("5");
-
-		
-		// EvDifferentialRequest request = new EvDifferentialRequest(
-		// 	startingHand, dealerUpCard, numExecutions, 20, PlayerAction.SPLIT, true);
-
-		// ExpectedValueResult experimentalStrategyResult = request.calculateExpectedValue();
-
-
-		// System.out.println();
-		// System.out.println("Basic Strategy simulation");
-		// System.out.println();
-
-
-		// request = new EvDifferentialRequest(
-		// 	startingHand, dealerUpCard, numExecutions, 20, PlayerAction.STAND, false);
-
-		// ExpectedValueResult basicStrategyResult = request.calculateExpectedValue();
-
-
-		// System.out.println("Results");
-		// System.out.println("Experimental: " + experimentalStrategyResult.getHandsWon() + 
-		// 	" won vs " + experimentalStrategyResult.getHandsLost() + " lost");
-		// System.out.println("Basic: " + basicStrategyResult.getHandsWon() + " won vs " + basicStrategyResult.getHandsLost() + " lost");
 	} //main
 
 } //BjsaToolApplication
--- bjsa_tool/src/main/java/com/dillonbrothers/bjsa_tool/controller/BjsaController.java
+++ bjsa_tool/src/main/java/com/dillonbrothers/bjsa_tool/controller/BjsaController.java
@@ -3,8 +3,9 @@ package com.dillonbrothers.bjsa_tool.controller;
 import java.math.BigInteger;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dillonbrothers.bjsa_tool.model.ExperimentalRequest;
@@ -26,13 +27,43 @@ public class BjsaController {
 
     @GetMapping("/v1/run-experimental-strategy")
     public ExpectedValueResponse calculateExpectedValue
-        (@RequestBody ExperimentalRequest request) {
+        (@RequestParam String dealerUpCard, @RequestParam BigInteger executionTimes, 
+            @RequestParam String action, @RequestParam String hand) {
+
+        PlayerAction playerAction = PlayerAction.STAND;
+
+        switch (action) {
+            case "stand":
+                break;
+            case "double":
+                playerAction = PlayerAction.DOUBLE;
+                break;
+            case "hit":
+                playerAction = PlayerAction.HIT;
+                break;
+            case "split":
+                playerAction = PlayerAction.SPLIT;
+                break;
+            default:
+                return null;
+        } //switch
         
+        ExpectedValueResponse response = service.calculateExpectedValue(
+            dealerUpCard, executionTimes, playerAction, hand);
+        
+        System.out.println(response.getExpectedValue());
+        return response;
+    } //calculateExpectedValue
+
+
+    @PostMapping("/v1/run-experimental-strategy")
+    public ExpectedValueResponse calculateExpectedValuePost (@RequestBody ExperimentalRequest request) {
+
         ExpectedValueResponse response = service.calculateExpectedValue(
             request.getDealerUpCard(), request.getExecutionTimes(), request.getAction(), request.getHand());
         
         System.out.println(response.getExpectedValue());
         return response;
-    } //calculateExpectedValue
+    } //calculateExpectedValuePost
 
 } //BjsaController
